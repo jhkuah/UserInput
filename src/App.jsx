c97f2985592d793e4159d8b9f91e637e1df4b779
jhkuah <122422751+jhkuah@users.noreply.github.com>
2023-06-23 00:20:26 +0800

    Deployed React app to GitHub pages

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import AddUser from "./components/Users/AddUser";
+import UserList from "./components/Users/UserList";
+
+function App() {
+  const [usersList, setUsersList] = useState([]);
+  const addUserHandler = (userName, userAge) => {
+    setUsersList((prevUsersList) => {
+      return [
+        ...prevUsersList,
+        { name: userName, age: userAge, id: Math.random().toString() },
+      ];
+    });
+  };
+  return (
+    <div>
+      <AddUser onAddUser={addUserHandler} />
+      {usersList.length > 0 && <UserList users={usersList} />}
+    </div>
+  );
+}
+
+export default App;
